021a9c80688a46a4016672919756afe530803ad4
Mehdi <webmahdi72@gmail.com>
2025-01-13 13:20:39 +0330

    Refactor getFileNameFromUrl to handle missing file extensions and null values." "Ensure fallback with UUID for invalid or incomplete URLs.

--- ketch/src/main/java/com/ketch/internal/utils/FileUtil.kt
+++ ketch/src/main/java/com/ketch/internal/utils/FileUtil.kt
@@ -1,5 +1,6 @@
 package com.ketch.internal.utils
 
+import android.net.Uri
 import android.os.Environment
 import android.webkit.URLUtil
 import java.io.File
@@ -13,9 +14,38 @@ internal object FileUtil {
         return File(file.absolutePath + ".temp")
     }
 
-    fun getFileNameFromUrl(url: String): String {
+    private fun getFileNameFromUrlUUID(url: String): String {
         val guessFileName = URLUtil.guessFileName(url, null, null)
         return UUID.randomUUID().toString() + "-" + guessFileName
+    }
+    /**
+     * Extracts a file name from a given URL string.
+     *
+     * This function attempts to extract the file name from the last segment of the URL path.
+     * If the URL is invalid, or if a valid file name with an extension cannot be found,
+     * a UUID-based file name is generated using [getFileNameFromUrlUUID]
+     *
+     * @param url The URL string to extract the file name from.
+     * @return The extracted file name, or a UUID-based file name if extraction fails.
+     *
+     * Examples:
+     *  - "https://example.com/path/to/file.txt" -> "file.txt"
+     *  - "https://example.com/image.jpg?query=param" -> "image.jpg"
+     *  - "https://example.com/path/to/file" -> "generated_uuid_string"
+     *  - "invalid_url" -> "generated_uuid_string"
+     */
+    fun getFileNameFromUrl(url: String): String {
+        if (!URLUtil.isValidUrl(url)) {
+            return getFileNameFromUrlUUID(url)
+        }
+        val uri = Uri.parse(url)
+        val fileName = uri.lastPathSegment ?: return getFileNameFromUrlUUID(url)
+        val fileExtension = fileName.substringAfterLast(".", "")
+        return if (fileExtension.isNotEmpty()) {
+            fileName
+        } else {
+            getFileNameFromUrlUUID(url)
+        }
     }
 
     fun getDefaultDownloadPath(): String {
